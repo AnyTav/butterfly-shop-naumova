f59d01b07506e0a6c8023c72dc235b017244456a
Anastasiya Naumova <aknaumova99@gmail.com>
2021-10-29 01:17:24 +0300

    Сделала страницу FAQ, сделала выпадающее меню

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,25 @@
-console.log('script work!');
+$(document).ready(function(){
+
+  $('.js-burger').on('click', function(){
+
+    $('.js-main-nav').toggleClass('main-nav-open');
+
+  });
+
+  let prevBtn;
+
+  $('.js-accordion-btn').on('click', function() {
+    console.log($(this));
+
+
+    if (prevBtn === $(this)[0]) {
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $('.js-accordion-btn').next().slideUp();
+    $(this).next().slideDown();
+    prevBtn = $(this)[0];
+  });
+
+});
