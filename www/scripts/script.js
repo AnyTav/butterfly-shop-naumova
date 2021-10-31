b20d68897756dd222727e559ee584571dbfa2fad
Anastasiya Naumova <aknaumova99@gmail.com>
2021-11-01 01:28:12 +0300

    Сделала страницу 'Отзовы'

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -22,4 +22,57 @@ $(document).ready(function(){
     prevBtn = $(this)[0];
   });
 
+
+  // Табы
+  $('.tabs-link').on('click', function(e) {
+    e.preventDefault();
+
+    let index = $(this).index('.tabs-link');
+    $('.tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    $('.contacts-content').removeClass('active');
+    $('.contacts-content').eq(index).addClass('active');
+
+
+  });
+
+
+
+
+
+  // Фильтр
+  $('.filter-link').on('click', function(event) {
+    event.preventDefault();
+
+    let linkType = $(this).data('type');
+    console.log(linkType);
+
+    $('.filter-link').removeClass('active');
+    $(this).addClass('active');
+
+    if (linkType === 'all') {
+      $('.portfolio-pic-wrap').show();
+      return;
+    }
+
+    $('.portfolio-pic-wrap').each(function(){
+      let itemType = $(this).data('type');
+
+      if (linkType === itemType) {
+        $(this).show();
+        return;
+      }
+
+      $(this).hide();
+    });
+
+  });
+
+  // Слайдер
+  // if ( $('.reviews-card-slider').length ) {
+  //   $('.reviews-card-slider').slick();
+  // }
+
+
 });
